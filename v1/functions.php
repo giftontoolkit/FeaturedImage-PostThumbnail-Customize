86fa7412bca91c02f50866453d6728287e10b1e3
giftontoolkit <giftontoolkit@gmail.com>
2025-03-02 06:37:38 +0500

    Add files via upload

--- v1/functions.php
+++ v1/functions.php
@@ -1,9 +1,9 @@
 <?php
 // Theme Setup - Featured-Image (Post Thumbnail)
-function gifton_theme_setup_featured_image() {
+function myTM_theme_setup_featured_image() {
 
     // Enable support for post thumbnails (featured-image)
     add_theme_support('post-thumbnails');
 }
-add_action('after_setup_theme', 'gifton_theme_setup_featured_image');
+add_action('after_setup_theme', 'myTM_theme_setup_featured_image');
 ?>
